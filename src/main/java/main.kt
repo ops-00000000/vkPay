3275108141dab7377f1f1df6f4542a48e8cfc6dd
Vladimir <belaevvova999@gmail.com>
2020-12-16 20:36:28 +0300

    Second commit

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,12 +1,9 @@
-fun main(){
+fun main() {
     val amount = 40000
     val coef = 0.075
-    val comm = amount * coef
-    if(comm<= 3500){
-        val minComm = 3500
-        println("Ваша комиссия: $minComm")
-    } else {
-        println("Ваша комиссия: $comm")
+    var comm = amount * coef
+    if (comm <= 3500) {
+        comm = 3500.0
     }
+    println("Ваша комиссия: $comm")
 }
-
